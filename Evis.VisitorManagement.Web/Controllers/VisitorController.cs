0d30fd4ec546ede22d11695ae558f02061848067
Shambhoo Kumar <kumarshambhoo84@gmail.com>
2016-01-29 19:14:44 +0400

    Scan Visitor Card

--- Evis.VisitorManagement.Web/Controllers/VisitorController.cs
+++ Evis.VisitorManagement.Web/Controllers/VisitorController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MODI;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,9 +11,31 @@ namespace Evis.VisitorManagement.Web.Controllers
     public class VisitorController : Controller
     {
         // GET: Visitor
+        [HttpGet]
         public ActionResult ScanVisitorCard()
         {
             return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult ScanVisitorCard(HttpPostedFileBase uploadFile)
+        {
+            try
+            {
+                if (uploadFile.ContentLength > 0)
+                {
+                    string filePath = Path.Combine(HttpContext.Server.MapPath("../Uploads"),
+                                                   Path.GetFileName(uploadFile.FileName));
+                    uploadFile.SaveAs(filePath);
+
+                    string imageText = ExtractTextFromImage(filePath);
+                }
+                return View();
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
         }
 
         public ActionResult AddVisitorManually()
@@ -23,6 +47,21 @@ namespace Evis.VisitorManagement.Web.Controllers
         {
             return View();
         }
+
+        #region "Private Method"
+
+        private string ExtractTextFromImage(string filePath)
+        {
+            Document modiDocument = new Document();
+            modiDocument.Create(filePath);
+            modiDocument.OCR(MiLANGUAGES.miLANG_ENGLISH);
+            MODI.Image modiImage = (modiDocument.Images[0] as MODI.Image);
+            string extractedText = modiImage.Layout.Text;
+            modiDocument.Close();
+            return extractedText;
+        }
+
+        #endregion
 
     }
 }
