5809fc05788240d71752fd8e938f344227a6ab4f
Vipin Raturi <vipinr@casemyway.com>
2016-01-29 19:46:10 +0530

    Visitors pages added.

---
+++ Evis.VisitorManagement.Web/Controllers/VisitorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Evis.VisitorManagement.Web.Controllers
+{
+    public class VisitorController : Controller
+    {
+        // GET: Visitor
+        public ActionResult ScanVisitorCard()
+        {
+            return View();
+        }
+
+        public ActionResult AddVisitorManually()
+        {
+            return View();
+        }
+
+        public ActionResult VisitorList()
+        {
+            return View();
+        }
+
+    }
+}
