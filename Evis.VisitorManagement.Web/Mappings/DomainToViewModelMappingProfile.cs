78bcdd6f181cb860f7500e7d48fb655c8d938a37
Junaid Ameen <junaidameen23@gmail.com>
2016-01-31 15:44:35 +0400

    Separated the angular controller files

---
+++ Evis.VisitorManagement.Web/Mappings/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Evis.VisitorManagement.DataProject.Model;
+using Evis.VisitorManagement.DataProject.Model.Entities;
+using Evis.VisitorManagement.DataProject.Model.Entities.Custom;
+using Evis.VisitorManagement.Web.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Evis.VisitorManagement.Web.Mapping
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get
+            {
+                return "DomainToViewModelMappings";
+            }
+        }
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<ApplicationUser, RegisterViewModel>();
+            Mapper.CreateMap<UserList, RegisterViewModel>();
+        }
+    }
+}
---
+++ Evis.VisitorManagement.Web/Scripts/Controllers/ManageUsersController.js
@@ -0,0 +1,10 @@
+﻿var app = angular.module('app', []);
+app.controller('manageUsercontroller', function ($scope, $http) {
+    $scope.allUsers = [];
+    $http({
+        method: 'GET',
+        url: '/Api/AccountApi/GetAllUsers'
+    }).success(function (result) {
+        $scope.allUsers = result;
+    });
+});
